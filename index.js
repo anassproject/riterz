f695286dde32e1d48e802a1a381a4e836c2953e4
anassproject <afahag53@gmail.com>
2020-06-14 15:44:38 +0100

    my app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 //const cors = require('cors')
 
 const app = express()
@@ -21,8 +22,12 @@ mongoose.connect(
 const PORT = process.env.PORT || 5000
 
 // for production
+
 if(process.env.NODE_ENV === 'production'){
 	app.use(express.static('client/build'))
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	})
 }
 
 app.listen(PORT, ()=> console.log('Server running!'))
